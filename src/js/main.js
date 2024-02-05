578bb57fe2a3979352c368bdd1fb1b511fda4d58
AzatF2020 <azatgaynutdinov2006@yandex.ru>
2024-02-05 16:05:22 +0300

    first commit to test

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import "./vendor/lazyload";
+import "./vendor/lazysized.unveilhooks";
+import "./vendor/libs/zoomple"
+
+import initFancyGallery from "./modules/fancyGallery";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initFancyGallery()
+});
---
+++ src/js/modules/fancyGallery.js
@@ -0,0 +1,55 @@
+import Swiper from "swiper";
+import {EffectFade, Controller} from "swiper/modules";
+import "swiper/css"
+import "swiper/css/effect-fade"
+
+Swiper.use([EffectFade, Controller])
+
+export default function initFancyGallery() {
+  const buttons = document.querySelectorAll(".js-gallery-button")
+  const galleries = document.querySelectorAll(".js-content-gallery")
+  
+  console.log(buttons)
+  
+  function enableGallerySliders(gallery) {
+    gallery.classList.add('--is-active')
+    
+    return new Swiper(gallery, {
+      effect: 'fade',
+      fadeEffect: true,
+      watchActiveIndex: true,
+    })
+  }
+  
+  function initController(galleryInstance, galleryController) {
+    const controllersButtons =  [...galleryController.children]
+    
+    controllersButtons.forEach((controllerButton, buttonIndex) => {
+      controllerButton.addEventListener("click", (event) => {
+        controllersButtons.forEach((button) =>  button.classList.remove("--is-active"))
+        
+        controllerButton.classList.add("--is-active")
+        galleryInstance.slideTo(buttonIndex)
+      })
+    })
+  }
+  
+  buttons.forEach((button) => {
+    const buttonAttr = button.dataset.gallery
+    button.addEventListener("click", () => {
+      console.log(button)
+
+      galleries.forEach((gallery) => {
+        const galleryController = gallery.querySelector(".js-gallery-controller")
+        const galleryAttr = gallery.dataset.gallery
+        
+        console.log(galleryAttr === buttonAttr, galleryAttr, buttonAttr )
+        
+        if(galleryAttr === buttonAttr) {
+          const galleryInstance = enableGallerySliders(gallery)
+          initController(galleryInstance, galleryController)
+        }
+      })
+    })
+  })
+}
